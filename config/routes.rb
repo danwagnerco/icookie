5ae693ab75c07b1b867ead89565c86ab55c447c8
Dan Wagner <danwagnerco@gmail.com>
2014-07-23 07:40:03 -0500

    adjust routes to link static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Icookie::Application.routes.draw do
   root "static_pages#home"
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/contact"
+  # get "static_pages/home"
+  # get "static_pages/about"
+  # get "static_pages/contact"
   resources :users
   resources :locations
+
+  # create friendlier URLs, like
+  # "icookie.com/about" rather than "icookie.com/static_pages/about"
+  StaticPagesController.action_methods.each do |action|
+    get "/#{action}", :to => "static_pages##{action}", :as => "#{action}"
+  end
+
 end

15595863b23bd7fc189d7b955d6250ad578d5700
Dan Wagner <danwagnerco@gmail.com>
2014-07-26 14:07:31 -0500

    update seeds to assign users to locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,46 @@
-User.create!(
+Location.create!(
+  [
+    {
+      :zip       => "60012",
+      :state     => "IL",
+      :city      => "Crystal Lake",
+      :county    => "McHenry",
+      :latitude  => 42.27,
+      :longitude => -88.32
+    },
+    {
+      :zip       => "60622",
+      :state     => "IL",
+      :city      => "Chicago",
+      :county    => "Cook",
+      :latitude  => 41.9,
+      :longitude => -87.68
+    },
+    {
+      :zip       => "60642",
+      :state     => "IL",
+      :city      => "Chicago",
+      :county    => "Cook",
+      :latitude  => 41.9,
+      :longitude => -87.66
+    },
+    {
+      :zip       => "60439",
+      :state     => "IL",
+      :city      => "Lemont",
+      :county    => "Cook",
+      :latitude  => 41.68,
+      :longitude => -87.98
+    }
+  ]
+)
+
+crystal_lake = Location.find_by(:zip => "60012")
+chicago_wp   = Location.find_by(:zip => "60622")
+chicago_wt   = Location.find_by(:zip => "60642")
+lemont       = Location.find_by(:zip => "60439")
+
+crystal_lake.users.create!(
   [
     {
       :first_name    => "Jess",
@@ -6,14 +48,24 @@ User.create!(
       :email         => "jess.caudillo@gmail.com",
       :zip           => "60012",
       :report_detail => "full"
-    },
+    }    
+  ]
+)
+
+chicago_wp.users.create!(
+  [
     {
       :first_name    => "Tom",
       :last_name     => "Riordan",
       :email         => "thomasjriordan@gmail.com",
       :zip           => "60622",
       :report_detail => "full"
-    },
+    }
+  ]
+)
+
+chicago_wt.users.create!(
+  [
     {
       :first_name    => "Kelly",
       :last_name     => "Turner",
@@ -27,7 +79,12 @@ User.create!(
       :email         => "danwagnerco@gmail.com",
       :zip           => "60642",
       :report_detail => "full"
-    },
+    }
+  ]
+)
+
+lemont.users.create!(
+  [
     {
       :first_name    => "Terese",
       :last_name     => "Turner",
@@ -48,6 +105,7 @@ User.create!(
       :email         => "emily.terese@gmail.com",
       :zip           => "60439",
       :report_detail => "full"
-    },
+    }
   ]
 )
+
